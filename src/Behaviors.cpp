7954ea342a32ae1c83f7d48a18fb8f967e59d852
Yusuf Simsek <yusuftariksimsek@gmail.com>
2023-12-07 18:51:29 -0500

    Implemented State Machine
    Co-authored-by: cmf2201 <cmf2201@users.noreply.github.com>

--- src/Behaviors.cpp
+++ src/Behaviors.cpp
@@ -3,6 +3,10 @@
 #include "Speed_controller.h"
 #include "Position_estimation.h"
 
+#define WALLDIST 10
+#define THRESHOLD_HIGH 45
+#define THRESHOLD_LOW 55
+
 //sensors
 Romi32U4ButtonA buttonA;
 
@@ -11,7 +15,13 @@ SpeedController robot;
 
 //Flags
 int target_room = -1;
-int in_corridoor = 0;
+int collisions = 0;
+int in_hallway = 0;
+int confirm_delivery = 0;
+float distR = 0;
+int distL = 0;
+int distF = 0;
+int april = 5; // ? Is NOTAG, but I'm confused why it doesn't accept NOTAG
 
 void Behaviors::Init(void)
 {
@@ -23,30 +33,49 @@ void Behaviors::Stop(void)
     robot.Stop();
 }
 
-int Behaviors::getTargetRoom(void)
+void Behaviors::setTargetRoom(void)
 {
-    return -1; // TODO Azura will fix this.
+    target_room = -1; // TODO Azura will add this.
 }
 
-int Behaviors::getInHallway(void)
+void Behaviors::setInHallway(void)
 {
-    return 0; // TODO Caleb, make this return 1 f it's in the hallway.
+    in_hallway = 0; // TODO Caleb, make this return 1 f it's in the hallway.
 }
 
-void Behaviors::SetFlags(void)
+void Behaviors::setConfirmDelivery(void)
 {
-    target_room = getTargetRoom();
-    in_corridoor = getInHallway();
+    confirm_delivery = 0; // TODO Azura will add this.
+}
+
+void Behaviors::setWallDistance(enum DIRECTION dir)
+{
+    if (dir == LEFT){
+        distL = 0;
+    }else if(dir == RIGHT){
+        distR = 0;
+    }else if(dir == FRONT){
+        distF = 0;
+    } // TODO Whoever's first do this
+}
+
+int Behaviors::collisionDetected(void)
+{
+    return 1; // TODO Whoever's first do this
+}
+
+int Behaviors::getAprilTag(void)
+{
+    return NOTAG; // TODO CALEB, return the ENUM
 }
 
 void Behaviors::Run(void)
 {
-    SetFlags();
     switch (robot_state)
     {
     case IDLE:
         if(buttonA.getSingleDebouncedRelease()){ 
-            robot_state = DRIVE; 
+            robot_state = FORWARD; 
             robot.Stop();             
         } 
         else { 
@@ -55,14 +84,105 @@ void Behaviors::Run(void)
         }   
         break;
     
-    case DRIVE:
-        robot_state = DRIVE;
-        // ! assignment
-        robot.Straight(100,10); //velocity, duration
-        // robot.Turn(180,0); //degrees, direction
-        robot.Curved(85.7125,114.2875,10); //velocity left, velocity right, duration
-        robot.Stop(); 
-        robot_state = IDLE;
+    case FORWARD:
+        setWallDistance(FRONT);
+        april = getAprilTag();
+
+        if(april == HALLWAY){
+            in_hallway = 1;
+        }
+
+        if(buttonA.getSingleDebouncedRelease() || april == HOME) {
+            robot_state = IDLE;
+            robot.Stop();
+        }else if(distF < WALLDIST){
+            robot_state = CHECKD_1;
+            robot.Stop();
+        }else if(distF < THRESHOLD_HIGH && distF > THRESHOLD_LOW){
+            robot_state = CHECKD_1;
+            robot.Stop();
+        }else{
+            robot.Run(100,100);
+        }
+        break;
+
+    case CHECKD_1:
+        setWallDistance(RIGHT);
+        april = getAprilTag();
+
+        if(april == target_room){
+            robot_state = KNOCK;
+            robot.Stop();
+        }else if(april == HALLWAY){
+            in_hallway = 1;
+        }else if(april == HOME){
+            robot.Stop();
+            robot_state = IDLE;
+        }else{
+            robot.Curved(50, -50, 1); // TODO Calibrate point turn
+            robot.Stop();
+            robot_state = CHECKD_2;
+        }
+        break;
+
+    case CHECKD_2:
+        setWallDistance(LEFT);
+        april = getAprilTag();
+
+        if(april == target_room){
+            robot_state = KNOCK;
+            robot.Stop();
+        }else if(april == HALLWAY){
+            in_hallway = 1;
+        }else if(april == HOME){
+            robot.Stop();
+            robot_state = IDLE;
+        }else{
+            if(distR > distL){
+                robot.Curved(50, -50, 1); // TODO Calibrate point turn, get direction
+                robot.Stop();
+                robot_state = FORWARD;
+            }else if(distL < distR){
+                robot.Curved(-50, 50, 1); // TODO Calibrate point turn, get direction
+                robot.Stop();
+                robot_state = FORWARD;
+            }else{
+                robot_state = IDLE;
+                robot.Stop();
+            }
+            robot.Stop();
+        }
+        break;
+
+    case KNOCK:
+        robot.Run(200, 200);
+        if(collisionDetected()){
+            robot_state = REVERSE;
+            robot.Stop();
+            collisions += 1;
+        }
+        break;
+    case REVERSE:
+        setWallDistance(FRONT);
+        if(distF < WALLDIST){
+            robot.Run(-100, -100);
+        }else{
+            if(collisions >= 3){
+                robot_state = WAIT;
+                robot.Stop();
+            }else{
+                robot_state = KNOCK;
+                robot.Stop();
+            }
+        }
+        break;
+    case WAIT:
+        setConfirmDelivery();
+        if(confirm_delivery){
+            target_room = -1;
+            robot.Stop();
+            robot_state = FORWARD;
+        }
         break;
     }
 }
