6cd38204baa313060398a5bc834e9c9895c8e6ca
cmf2201 <73976000+cmf2201@users.noreply.github.com>
2023-12-07 18:47:42 -0500

    Distance Sensor Working

--- src/Behaviors.cpp
+++ src/Behaviors.cpp
@@ -2,6 +2,7 @@
 #include "Behaviors.h"
 #include "Speed_controller.h"
 #include "Position_estimation.h"
+#include "IR_sensor.h"
 
 //sensors
 Romi32U4ButtonA buttonA;
@@ -9,9 +10,13 @@ Romi32U4ButtonA buttonA;
 //motor-speed controller
 SpeedController robot;
 
+//IR sensor
+IRsensor SharpIR;
+
 void Behaviors::Init(void)
 {
     robot.Init();
+    SharpIR.Init();
 }
 
 void Behaviors::Stop(void)
@@ -35,13 +40,14 @@ void Behaviors::Run(void)
         break;
     
     case DRIVE:
-        robot_state = DRIVE;
-        // ! assignment
-        robot.Straight(100,10); //velocity, duration
-        // robot.Turn(180,0); //degrees, direction
-        robot.Curved(85.7125,114.2875,10); //velocity left, velocity right, duration
-        robot.Stop(); 
-        robot_state = IDLE;
+        if(buttonA.getSingleDebouncedRelease()){ 
+            robot_state = IDLE; 
+            robot.Stop();             
+        } else {
+            robot_state = DRIVE;
+            Serial.print("Distance: ");
+            Serial.println(SharpIR.ReadData());
+        }
         break;
     }
 }
